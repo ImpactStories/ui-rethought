e2e33c2cd7e0fa431d6142b28becd84a1fb83659
Roland Jee <onlyahost@gmail.com>
2022-08-28 12:43:23 +0900

    reformatted

--- utils/clipboard/utils/clipboardInputDataToPlain.ts
+++ utils/clipboard/utils/clipboardInputDataToPlain.ts
@@ -1,36 +1,41 @@
-import { IClipboardInputBlock, IClipboardInputData, TClipboardInputBlockType } from "../interface";
+import {
+  IClipboardInputBlock,
+  IClipboardInputData,
+  TClipboardInputBlockType,
+} from "../interface";
 
 const blockDictionary: {
-    [key in TClipboardInputBlockType]: string;
-  } = {
-    block: "",
-    h1: "#",
-    h2: "##",
-    h3: "###",
-    h4: "####",
-    uli: "-",
-    oli: "-",
-    img: "img",
-  };
+  [key in TClipboardInputBlockType]: string;
+} = {
+  block: "",
+  h1: "#",
+  h2: "##",
+  h3: "###",
+  h4: "####",
+  uli: "-",
+  oli: "-",
+  img: "img",
+};
 
 const buildMarkdownElement = (dataItem: IClipboardInputBlock): string => {
-    const markdownTag = blockDictionary[dataItem.type];
-    const element = `${markdownTag} ${dataItem.children} \\`;
-    return element;
-  };
+  const markdownTag = blockDictionary[dataItem.type];
+  const element = `${markdownTag} ${dataItem.children} \\`;
+  return element;
+};
 
 const generateBlocks = (data: IClipboardInputData): string => {
-    const blocksMarkdown = data
-      .map((dataItem) => {
-        const element = buildMarkdownElement(dataItem);
-        return element;
-      })
-      .join("");
-    return blocksMarkdown;
-  };  
+  const blocksMarkdown = data
+    .map((dataItem) => {
+      const element = buildMarkdownElement(dataItem);
+      return element;
+    })
+    .join("");
+  return blocksMarkdown;
+};
 
-export const clipboardInputDataToPlain = (data: IClipboardInputData): string => {
-    const blocks = generateBlocks(data);
-    return blocks;
-  };
-  
+export const clipboardInputDataToPlain = (
+  data: IClipboardInputData
+): string => {
+  const blocks = generateBlocks(data);
+  return blocks;
+};
