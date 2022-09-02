56020fa1efd68709c2855098ed4926123124172a
Roland Jee <onlyahost@gmail.com>
2022-09-03 08:37:57 +0900

    remove comments

--- components/container/container.stories.tsx
+++ components/container/container.stories.tsx
@@ -3,30 +3,17 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Container } from "./index";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Base/Container",
   component: Container,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
 } as ComponentMeta<typeof Container>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Container> = (args) => (
   <Container {...args} />
 );
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {};
-
-export const Secondary = Template.bind({});
-Secondary.args = {};
-
-export const Large = Template.bind({});
-Large.args = {};
-
-export const Small = Template.bind({});
-Small.args = {};
