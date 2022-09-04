769b494cf1e5268f858720f157f3b779ebac9e24
Roland Jee <onlyahost@gmail.com>
2022-09-05 08:54:39 +0900

    documentation config

--- configuration/config.ts
+++ configuration/config.ts
@@ -8,14 +8,33 @@
  */
 
 export const Theme = {
+  /**
+   * In many components you can interact with width, height, margins, paddings, etc.
+   * You always can define these values using arbitrary values in px, etc. or
+   * you can use values based on a multiplier.
+   *
+   * The values you can use are multipliers of this base value.
+   * For example using "3x" will result in 6px (2: baseValue * 3: multiplier)
+   */
   gridBaseValue: 2,
 
+  /**
+   * There is no requirement for your naming patterns,
+   * but we recommend using a {colorName}-{colorValue} combination.
+   *
+   * This keeps you more flexible than for example grey-dark, grey-darker, ...
+   */
   colors: {
     "grey-100": "#eee",
     "grey-200": "#eee",
     "red-100": "#FF0000",
   },
 
+  /**
+   * Define as many device sizes as you need.
+   * See the utility functions isMinWidth and isMaxWidth
+   * to set media queries based on these device sizes
+   */
   deviceSizes: {
     mobile: "450px",
     tablet: "768px",
@@ -23,6 +42,11 @@ export const Theme = {
   },
 
   fonts: {
+    /**
+     * Your primary and secondary font family will automatically
+     * be used in your typogrpahy component and wherever fonts
+     * are used in our components.
+     */
     primary: {
       "Noto Sans": [
         {
@@ -41,6 +65,14 @@ export const Theme = {
         },
       ],
     },
+
+    /**
+     * Fonts usually are quite large in width and
+     * also designwise it mostly makes sense to use a maximum of two font families
+     *
+     * Of course there are cases where more families make sense,
+     * but think twice before embedding additional fonts.
+     */
     additionals: {
       "Noto Sans": [
         {
@@ -49,7 +81,7 @@ export const Theme = {
           src: "assets/fonts/noto/noto-sans-v27-latin-700.woff2",
         },
       ],
-    }
+    },
   },
 };
 
